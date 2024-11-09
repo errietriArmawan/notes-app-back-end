31f597b446ad30a2c3e886bf72d48def52571e07
Errie Tri Armawan <errietri555@gmail.com>
2024-11-09 17:13:45 +0700

    membuat folder menjadi repo dan menmbahkan .file gitignore

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import daStyle from 'eslint-config-dicodingacademy';
+
+/** @type {import('eslint').Linter.Config[]} */
+const config = [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  daStyle,
+];
+
+export default config;
+
